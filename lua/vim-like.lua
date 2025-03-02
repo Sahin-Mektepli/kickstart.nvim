820bed5ee2d76338bc6c5e1503a3dcf266292d8f
sahin-mektepli <sahin.mektepli@ozu.edu.tr>
2025-03-02 23:28:06 +0300

    some small stuff

--- lua/vim-like.lua
+++ lua/vim-like.lua
@@ -1,3 +1,6 @@
 -- This will have my own additions to the configuration of neovim. I will have vanilla vim configs that otherwise would have been in a vimrc file here.
 -- set hybrid numbers
-vim.cmd('set number relativenumber')
+vim.cmd 'set number relativenumber'
+
+-- Runpy command that I like to set.
+vim.api.nvim_create_user_command('Runpy', '!python3 %', {})
