22f27de19813ca669777ab630211af282d27ab6d
sahin-mektepli <sahin.mektepli@ozu.edu.tr>
2025-04-12 19:07:01 +0300

    simple code runner commands
    I don't know how to use options yet, but I want to be able to specify
    the version of C (and possibly cpp) when I run RunC

--- lua/vim-like.lua
+++ lua/vim-like.lua
@@ -4,3 +4,14 @@ vim.cmd 'set number relativenumber'
 
 -- Runpy command that I like to set.
 vim.api.nvim_create_user_command('Runpy', '!python3 %', {})
+-- vim.api.nvim_create_user_command('RunC', function ()
+--   local filename = vim.fn.expand('%:t:r') --% is the complete path, :t trims just to the filename, :r rids the extension
+--   local cmd = string.format('terminal gcc %% -o %s ')
+-- end
+vim.api.nvim_create_user_command('RunC', '!gcc % -o %:r && ./%:r', {})
+--TODO: make this so that it takes the C version as its argument
+vim.keymap.set('n', '<leader>rc', ':RunC<CR>', { silent = true })
+vim.keymap.set('n', '<leader>rp', ':Runpy<CR>', { silent = true })
+
+-- I use this so often.
+vim.keymap.set('n', '<leader>tv', ':vert split | terminal<CR>')
