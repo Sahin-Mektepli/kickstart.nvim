99cbab9965a9ffff56652e14c265d8d79dab6a09
sahin-mektepli <sahin.mektepli@ozu.edu.tr>
2025-04-17 18:31:31 +0300

    added markdown_preview

--- init.lua
+++ init.lua
@@ -743,7 +743,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = false } -- this was true for cpp as well.
+        local disable_filetypes = { c = false, cpp = false } -- this was true for cpp as well.
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
